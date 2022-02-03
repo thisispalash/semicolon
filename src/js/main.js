648549910d181e7c56d0961d85780f5dce79a4fb
Palash A <palash.agarwal.96@gmail.com>
2022-02-04 01:26:15 +0530

    lets find out

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,13 @@ checkUser = async () => {
     window.location.pathname === '/'
     || window.location.pathname === 'semicolon' // for gh-pages
   ) { return }
+  console.log(window.location.pathname)
   const current = Moralis.User.current()
-  if (!current) {
-    console.log('no user signed in')
-    location.href = '/'
-  } else {
-    console.log('user signed in')
-    console.log(current)
-  }
+  // if (!current) {
+  //   console.log('no user signed in')
+  //   location.href = '/'
+  // } else {
+  //   console.log('user signed in')
+  //   console.log(current)
+  // }
 }
