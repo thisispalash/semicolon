b6d725912e4f64300a7f24e8d8ec0688d2c06d94
Palash A <palash.agarwal.96@gmail.com>
2022-02-04 01:33:43 +0530

    gotcha

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,15 @@
 checkUser = async () => {
   if (
-    window.location.pathname === '/'
-    || window.location.pathname === 'semicolon' // for gh-pages
+    window.location.pathname === '/' // for local dev
+    || window.location.pathname === '/semicolon/' // for gh-pages
   ) { return }
-  console.log(window.location.pathname)
+  
   const current = Moralis.User.current()
-  // if (!current) {
-  //   console.log('no user signed in')
-  //   location.href = '/'
-  // } else {
-  //   console.log('user signed in')
-  //   console.log(current)
-  // }
+  if (!current) {
+    console.log('no user signed in')
+    location.href = '/'
+  } else {
+    console.log('user signed in')
+    console.log(current)
+  }
 }
