18c8d4d12ecb5c405cdcf3ef66048c1e0f0f4e9f
Palash A <palash.agarwal.96@gmail.com>
2022-02-05 15:38:01 +0530

    add localStorage

--- src/js/reader.js
+++ src/js/reader.js
@@ -1,5 +1,9 @@
 function loadStory() {
   console.log('todo query')
+  let tokenID = localStorage.getItem('tokenID')
+  let heading = 'Story ID: ' + tokenID
+  $('#heading').html(heading)
+  // TODO lookup id on the network
 }
 
 function remint() {
