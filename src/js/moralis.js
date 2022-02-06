9c265896c43e36d235977d576d203db9a79e5e88
Palash A <palash.agarwal.96@gmail.com>
2022-02-06 21:09:05 +0530

    display nft from moralis

--- src/js/moralis.js
+++ src/js/moralis.js
@@ -13,4 +13,61 @@ async function login() {
   }
 }
 
+function getMetadata(arr) {
+  var meta = []
+  arr.forEach(item => {
+    $.ajax({
+      url: item.metadata,
+      success: (res) => { meta.push(res) }
+    })
+  })
+  return meta
+}
+
+function displayNFTs(_id, arr) {
+  let parent = $('#'+_id)
+  if (!arr) {
+    let div = `
+      <div class="row">
+        <div class="container">
+          No Stories minted
+        </div>
+      </div>
+    `
+    parent.appendChild(div)
+    return;
+  }
+
+  arr.forEach( (item) => {
+    let div = `
+      <div class="row">
+        <div class="card bg-dark mb-3">
+          <img src="${item.img}" class="card-img-top">
+        </div>
+      </div>
+    `
+    parent.appendChild(div)
+  })
+}
+
+async function checkNFTs() {
+  const NFTs = await Moralis.Web3API.account.getNFTs({ chain: 'mumbai' });
+  var mints = [], remints = [];
+  if(!NFTs.total) {
+    NFTs.result.forEach(nft => {
+      if (nft.symbol === "SMF") mints.push(nft);
+      if (nft.symbol === "reSMF") remints.push(nft);
+    });
+
+    mint_meta = getMetadata(mints)
+    remint_meta = getMetadata(remints)
+
+    // Display NFT
+    displayNFTs('ownmint', mint_meta)
+    displayNFTs('remint', remint_meta)
+
+  }
+}
+
+
 console.log('end `moralis.js`')
