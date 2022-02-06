a5bb28463157e56f09993ec357a2466bb88896ff
Palash A <palash.agarwal.96@gmail.com>
2022-02-07 04:58:57 +0530

    a little more practical

--- src/js/list.js
+++ src/js/list.js
@@ -13,7 +13,6 @@ async function initPage() {
   let code = parent.html(), flag = true
 
   NFTs.result.forEach(token => {
-    console.log(token)
     let obj = JSON.parse(token.metadata)
     let h = Number(obj.hue), s = Number(obj.sat), l = Number(obj.lum)
     // ± 10% range in hue then display
@@ -23,32 +22,33 @@ async function initPage() {
         s-20 <= sat && sat <= s+20
         && l-20 <= lum && lum <= l+20
       ) {
-        let row = `
-          <div class="row">
-            <div class="card mb-3 bg-dark">
-              <div class="row g-0">
-                <div class="col-md-4">
-                  <img src="${obj.img}" class="img-fluid rounded-start m-2" width="300" height="300">
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <p class="card-text">
-                      <p>Story Numbah ${token.token_id}</p>
-                      <a class="btn btn-lg btn-outline-light" onClick="readStory(${token.token_id})">Read Story</a>
-                    </p>
-                    <p class="card-text">
-                      <small class="text-muted">
-                        hue: ${h}, saturation: ${s}, lightness: ${l}
-                      </small>
-                    </p>
-                  </div>
+        // too few mints for this
+      }
+      let row = `
+        <div class="row">
+          <div class="card mb-3 bg-dark">
+            <div class="row g-0">
+              <div class="col-md-4">
+                <img src="${obj.img}" class="img-fluid rounded-start m-2" width="300" height="300">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <p class="card-text">
+                    <p>Story Numbah ${token.token_id}</p>
+                    <a class="btn btn-lg btn-outline-light" onClick="readStory(${token.token_id})">Read Story</a>
+                  </p>
+                  <p class="card-text">
+                    <small class="text-muted">
+                      hue: ${h}, saturation: ${s}, lightness: ${l}
+                    </small>
+                  </p>
                 </div>
               </div>
             </div>
           </div>
-        `; code += row
-        flag = false
-      }
+        </div>
+      `; code += row
+      flag = false
     }
   });
   if (flag) {
